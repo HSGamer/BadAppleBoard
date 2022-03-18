7adaec7f331953453bff3fecb86e1906663370f9
HSGamer <huynhqtienvtag@gmail.com>
2022-03-18 09:20:29 +0700

    force use UTF-8

--- src/main/java/me/hsgamer/badappleboard/BadAppleBoard.java
+++ src/main/java/me/hsgamer/badappleboard/BadAppleBoard.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,7 +42,7 @@ public final class BadAppleBoard extends BasePlugin {
 
     private List<Frame> load(InputStream inputStream) {
         List<Frame> list = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             int i = 0;
             String line = bufferedReader.readLine();
             Frame frame = null;
